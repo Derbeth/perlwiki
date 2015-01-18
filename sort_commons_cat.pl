b2098c0f528d2f87e36a14c823dd0b479bd24292
Derbeth <derbeth@interia.pl>
2015-01-18 22:57:09 +0100

    sort_commons_cat: exit with non-zero code when interrupted

--- sort_commons_cat.pl
+++ sort_commons_cat.pl
@@ -62,7 +62,7 @@ my %done;
 unlink $donefile if ($clean && -e $donefile);
 read_hash_loose($donefile, \%done);
 
-$SIG{INT} = $SIG{TERM} = $SIG{QUIT} = sub { save_results(); exit; };
+$SIG{INT} = $SIG{TERM} = $SIG{QUIT} = sub { save_results(); exit 1; };
 
 # ======= main
 
