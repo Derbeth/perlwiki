e2fe6459978406378aee4da3b778cd4db0cf1cc4
Derbeth <derbeth@interia.pl>
2015-03-07 11:12:27 +0100

    test Derbeth::Commons::detect_pronounced_word

--- test/commons.pl
+++ test/commons.pl
@@ -1,36 +1,61 @@
 #!/usr/bin/perl -w
 
 use strict;
+use utf8;
 
 use Derbeth::Commons;
 use Encode;
+use File::Slurp;
+use Test::Assert ':all';
 
-my $input='testdata/commons.ini';
-open(IN, $input) or die "cannot read $input";
+test_word_pronounced_in_file();
+test_detect_pronounced_word();
 
-my $tests=0;
-my $line=0;
-while(<IN>) {
-	++$line;
-	chomp;
-	next if (/^#/ || !/\w/);
-	$_ = decode_utf8($_);
-	my ($key,$value) = split(/=/, $_);
-	my ($lang, $input_file) = split(/\|/, $key);
-	my @expected = split(/\|/, $value);
+sub test_word_pronounced_in_file {
+	my $input='testdata/commons.ini';
+	open(IN, $input) or die "cannot read $input";
 
-	die "parse error in $input:$line" unless($lang && $input_file);
+	my $tests=0;
+	my $line=0;
+	while(<IN>) {
+		++$line;
+		chomp;
+		next if (/^#/ || !/\w/);
+		$_ = decode_utf8($_);
+		my ($key,$value) = split(/=/, $_);
+		my ($lang, $input_file) = split(/\|/, $key);
+		my @expected = split(/\|/, $value);
 
-	my ($file, @words) = word_pronounced_in_file("File:$input_file", $lang);
-	if ($#words != $#expected) {
-		die encode_utf8("$input:$line: expected '@expected' received '@words'");
-	}
-	for (my $i=0; $i<=$#words; ++$i) {
-		if ($words[$i] ne $expected[$i]) {
-			die encode_utf8("$input:$line: expected '$expected[$i]' received '$words[$i]'");
+		die "parse error in $input:$line" unless($lang && $input_file);
+
+		my ($file, @words) = word_pronounced_in_file("File:$input_file", $lang);
+		if ($#words != $#expected) {
+			die encode_utf8("$input:$line: expected '@expected' received '@words'");
 		}
+		for (my $i=0; $i<=$#words; ++$i) {
+			if ($words[$i] ne $expected[$i]) {
+				die encode_utf8("$input:$line: expected '$expected[$i]' received '$words[$i]'");
+			}
+		}
+		++$tests;
 	}
-	++$tests;
+	close(IN);
+
+	print "\nword_pronounced_in_file: $tests tests succeeded\n";
 }
 
-print "\nAll $tests tests succeeded\n";
+sub test_detect_pronounced_word {
+	my $checked = 0;
+
+	$checked += _check_detected('zh-mofang.txt', 'zh', ['模仿']);
+
+	print "detect_pronounced_word: $checked tests succeeded\n";
+}
+
+sub _check_detected {
+	my ($input_file, $lang_code, $expected_arr) = @_;
+	my $input = read_file("testdata/commons-detect/$input_file", binmode => ':utf8');
+	my @actual = Derbeth::Commons::_detect($lang_code, $input);
+	assert_deep_equals $expected_arr, \@actual;
+	return 1;
+}
